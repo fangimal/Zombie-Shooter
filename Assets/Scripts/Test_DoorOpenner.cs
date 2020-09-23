8fd74fbe02f418f82a75edadeb27767e2a80d467
fangimal <gimalfan@gmail.com>
2020-09-23 19:56:00 +0300

    Добавил тестовую сцену для проверки над динамическим перестроении траектории врагов при изменении препятствии. Создал блок управляемый скриптом (опускается поднимается пробелом)

---
+++ Assets/Scripts/Test_DoorOpenner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_DoorOpenner : MonoBehaviour
+{
+    bool open = false;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            if (open)
+            {
+                this.transform.Translate(0, -3, 0);
+            }
+            else
+                this.transform.Translate(0, 3, 0);
+            open = !open;
+        }
+    }
+}
